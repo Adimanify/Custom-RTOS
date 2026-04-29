45e9ad17679f7bcd110153ad66db457b6e8a9eb5
Aditya Srivastava <adityasrivastava1824@gmail.com>
2026-04-29 20:37:06 +0530

    Add IRQ enable, disable, and pending functions

--- rtos/hw/interrupts.c
+++ rtos/hw/interrupts.c
@@ -1,1 +1,14 @@
+#include "./include/interrupts.h"
+
+void irq_enable(unsigned int irq_mask){
+    IRQ_ENABLE1 = irq_mask;
+}
+
+void irq_disable(unsigned int irq_mask){
+    IRQ_DISABLE1 = irq_mask;
+}
+
+int irq_is_pending(unsigned int irq_mask) {
+    return (IRQ_PENDING1 & irq_mask) != 0;
+}
 
