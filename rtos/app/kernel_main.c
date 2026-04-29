f92283e0e85d050dc21c051331eb3167e9e6a4a1
Aditya Srivastava <adityasrivastava1824@gmail.com>
2026-04-29 20:28:09 +0530

    Implement basic UART communication in kernel_main

--- rtos/app/kernel_main.c
+++ rtos/app/kernel_main.c
@@ -1,1 +1,19 @@
+#include "../rtos/rtos.h"
+#include "../hw/include/gpio.h"
+#include "../hw/include/system_timer.h"
+#include "../hw/include/interrupts.h"
+#include "../hw/include/uart.h"
+
+#define LED_PIN 4   // GPIO17 = pin 11 on RPi header
+
+
+void kernel_main(void) {
+    uart_init();
+    uart_puts("Hello, bare-metal world!\n");
+
+    while (1) {
+        char c = uart_getc(); // Wait for input
+        uart_putc(c);         // Echo back
+    }
+}
 
