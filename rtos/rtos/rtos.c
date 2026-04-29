5b476ea95245e088ec8e9070fde280580ad7056d
Aditya Srivastava <adityasrivastava1824@gmail.com>
2026-04-29 20:38:49 +0530

    Add task creation and RTOS start functions
    Implement basic task creation and starting functionality.

--- rtos/rtos/rtos.c
+++ rtos/rtos/rtos.c
@@ -1,1 +1,19 @@
+#include "rtos.h"
+#include "gpio.h"
+
+static task_func_t first_task = 0;
+
+void task_create(task_func_t func, int priority) {
+    // For now we ignore priority and just store one task
+    first_task = func;
+}
+
+void rtos_start(void) {
+    if (first_task) {
+        first_task();  // just jump into the task function
+    }
+
+    // If no task defined, loop forever
+    while (1);
+}
 
