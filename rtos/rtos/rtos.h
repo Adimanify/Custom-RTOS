ac2fb743b72d92fb303bcbec7266e8cb8b07d23b
Aditya Srivastava <adityasrivastava1824@gmail.com>
2026-04-29 20:39:05 +0530

    Add RTOS header file with task management functions

--- rtos/rtos/rtos.h
+++ rtos/rtos/rtos.h
@@ -1,1 +1,13 @@
+#ifndef RTOS_H
+#define RTOS_H
 
+// Forward declaration of task function
+typedef void (*task_func_t)(void);
+
+void task_create(task_func_t func, int priority);
+void rtos_start(void);
+
+// Stub for delay (just a busy loop for now)
+void task_delay(unsigned int ms);
+
+#endif
